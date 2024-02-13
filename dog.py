07d63f287bae135bced0bf5fa55adedcfe787d66
lmraine <55120024+lmraine@users.noreply.github.com>
2024-02-13 09:58:44 -0600

    dog class example

---
+++ dog.py
@@ -0,0 +1,8 @@
+class Dog():
+    def __init__(self):
+        self.name = "Unkown"
+        self.age = 0
+        self.breed = "heeler"
+        
+    def speak(self):
+        print("Woof")
---
+++ main.py
@@ -0,0 +1,30 @@
+import dog
+
+abby = dog.Dog()
+abby.name = "Abby"
+abby.age = 7
+abby.breed = "unknown"
+
+bandit = dog.Dog()
+bandit.name = "Bandit"
+bandit.age = 40
+
+print(bandit.age + abby.age)
+
+#PROCEDURAL 
+'''name = "abby"
+breed = "unknown"
+color = "tan"
+age = 7
+owner = "Lisa"
+
+name2 = "bandit"
+breed = "heeler"
+
+def speak():
+    print("Woof")
+
+def walk():
+    print("walk")'''
+
+
